0be868357987fdfb98b17e4bd993f93ed4db7e46
Mohammed Billoo <mab@mab-labs.com>
2023-08-23 17:59:57 -0400

    Fix python typos

--- .github/workflows/push-to-tps.py
+++ .github/workflows/push-to-tps.py
@@ -17,11 +17,14 @@ oauth_data['client_id'] = api_key
 oauth_data['client_secret'] = api_secret
 
 r = requests.post('https://kc.torizon.io/auth/realms/ota-users/protocol/openid-connect/token', headers=oauth_headers, data=oauth_data)
-response = r.text
-access_token = response["access_token"]
+response = r.json()
+access_token = response['access_token']
 
 secure_headers = dict()
-secure_headers['Authorization'] = 'Bearer ' + access_token
+secure_headers = {
+        'Authorization': 'Bearer ' + access_token
+}
+
 secure_data = {
         'offset': 0,
         'limit': 10
