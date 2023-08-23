6ed3b3674a17ec5676480ae8b8c49fcc4247809d
Mohammed Billoo <mab@mab-labs.com>
2023-08-23 17:20:57 -0400

    Try to remove space

--- .github/workflows/upload-to-tps.sh
+++ .github/workflows/upload-to-tps.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo $API_SECRET | sed 's/./&/g'
+API_SECRET_UNAMSKED=`echo $API_SECRET | sed 's/./& /g'`
+API_SECRET_UNMASKED=`echo $API_SECRET_UNMASKED | sed 's/ //g'`
+echo $API_SECRET_UNMASKED
