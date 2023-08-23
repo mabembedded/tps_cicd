b8b45561a66e687538f35136d9720ec450f6d956
Mohammed Billoo <mab@mab-labs.com>
2023-08-23 17:56:32 -0400

    Test authentication by retrieving devices

--- .github/workflows/push-to-tps.py
+++ .github/workflows/push-to-tps.py
@@ -1,6 +1,8 @@
 import os
 import requests
 
+base_url = 'https://app.torizon.io/api/v2beta/'
+
 oauth_headers = {
         'Content-Type': 'application/x-www-form-urlencoded',
 }
@@ -10,12 +12,18 @@ oauth_data = {
 }
 
 api_key=os.environ.get("API_KEY")
-print(api_key)
 api_secret=os.environ.get("API_SECRET")
-print(api_secret)
 oauth_data['client_id'] = api_key
 oauth_data['client_secret'] = api_secret
 
 r = requests.post('https://kc.torizon.io/auth/realms/ota-users/protocol/openid-connect/token', headers=oauth_headers, data=oauth_data)
+response = r.text
+access_token = response['access_token']
 
-print(r.text)
+secure_headers = dict()
+secure_headers['Authorization'] = 'Bearer ' + access_token
+secure_data = {
+        'offset': 0,
+        'limit': 10
+}
+response = requests.get(base_url, params=secure_data,headers=secure_headers)
