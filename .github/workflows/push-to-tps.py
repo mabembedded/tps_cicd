0cbf16ffdd0de328d2572527dbb4e72b349cc115
Mohammed Billoo <mab@mab-labs.com>
2023-08-23 17:50:06 -0400

    Fix typo for client secret

--- .github/workflows/push-to-tps.py
+++ .github/workflows/push-to-tps.py
@@ -14,7 +14,7 @@ print(api_key)
 api_secret=os.environ.get("API_SECRET")
 print(api_secret)
 oauth_data['client_id'] = api_key
-oauth_data['api_secret'] = api_secret
+oauth_data['client_secret'] = api_secret
 
 r = requests.post('https://kc.torizon.io/auth/realms/ota-users/protocol/openid-connect/token', headers=oauth_headers, data=oauth_data)
 
